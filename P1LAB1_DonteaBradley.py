20262f57113daeaa009142aca91ec3c8cd30fb75
ynnodx <bradleyd5452@student.faytechcc.edu>
2026-06-14 20:51:58 -0400

    Add files via upload

---
+++ P1LAB1_DonteaBradley.py
@@ -0,0 +1,10 @@
+# Dontea Bradley
+# 06/14/2026
+# P1LAB1
+# This program gets information from the user and displays output.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!")
+print("Welcome to CTI-110.")
